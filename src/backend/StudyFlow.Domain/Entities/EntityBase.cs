589b942efd22074d7d213ba874e76b860f6a9e41
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-02-08 17:24:05 -0300

    fea: add user entity and entity base.

---
+++ src/backend/StudyFlow.Domain/Entities/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace StudyFlow.Domain.Entities
+{
+    public class EntityBase
+    {
+        public long Id { get; set; }
+        public bool Active { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
