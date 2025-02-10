5171bc43b49b03454315536ff0342e4ec05da956
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-02-10 08:56:02 -0300

    feat: add register user integration tests.

---
+++ tests/WebApi.Test/StudyFlowClassFixture.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+
+namespace WebApi.Test
+{
+    public class StudyFlowClassFixture : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _httpClient;
+
+        public StudyFlowClassFixture(CustomWebApplicationFactory factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+
+        protected async Task<HttpResponseMessage> DoPost(string endpoint, object request)
+        {
+            return await _httpClient.PostAsJsonAsync(endpoint, request);
+        }
+    }
+}
---
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -0,0 +1,51 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using StudyFlow.Exceptions;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.User.Register
+{
+    public class RegisterUserTest(CustomWebApplicationFactory factory) : StudyFlowClassFixture(factory)
+    {
+        private readonly string _endpoint = "User";
+
+        [Fact]
+        public async Task Success()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            var response = await DoPost(_endpoint, request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            responseData.RootElement.GetProperty("name").GetString().Should().NotBeNullOrWhiteSpace().And.Be(request.Name);
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task Error_Empty_Name(string culture)
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+
+            var response = await DoPost(_endpoint, request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+            using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+            var expectedMessage = ResourceMessagesException.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(culture));
+        }
+    }
+}
