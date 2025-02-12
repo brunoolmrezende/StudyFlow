f0baf86ab3c1a0ad8f1428656871548a38b0e948
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-02-12 20:49:02 -0300

    feat: add integration tests for DoLogin.

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using CommonTestUtilities.Entities;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,6 +9,9 @@ namespace WebApi.Test
 {
     public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     {
+        private StudyFlow.Domain.Entities.User _user = default!;
+        private string _password = string.Empty;
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseEnvironment("Test")
@@ -27,11 +31,31 @@ namespace WebApi.Test
                         options.UseInMemoryDatabase("InMemoryDbForTesting");
                         options.UseInternalServiceProvider(provider);
                     });
+
+                    using var scope = services.BuildServiceProvider().CreateScope();
+
+                    var database = scope.ServiceProvider.GetRequiredService<StudyFlowDbContext>();
+
+                    database.Database.EnsureDeleted();
+
+                    StartDatabase(database);
                 });
+    
+        }
 
+        public string GetEmail() => _user.Email;
+        public string GetPassword() => _password;
+        public string GetUserName() => _user.Name;
 
+        private void StartDatabase(StudyFlowDbContext dbContext)
+        {
+            (_user, _password) = UserBuilder.Build();
 
-            
+            dbContext.Database.EnsureCreated();
+
+            dbContext.Users.Add(_user);
+
+            dbContext.SaveChanges();
         }
     }
 }
---
+++ tests/WebApi.Test/Login/DoLogin/DoLoginTest.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using StudyFlow.Communication.Requests;
+using System.Net;
+using System.Text.Json;
+
+namespace WebApi.Test.Login.DoLogin
+{
+    public class DoLoginTest : StudyFlowClassFixture
+    {
+        private readonly string _endpoint = "login";
+
+        private readonly string _email;
+        private readonly string _password;
+        private readonly string _userName;
+
+        public DoLoginTest(CustomWebApplicationFactory factory) : base(factory)
+        {
+            _email = factory.GetEmail();
+            _password = factory.GetPassword();
+            _userName = factory.GetUserName();
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            var request = new RequestDoLoginJson
+            {
+                Email = _email,
+                Password = _password,
+            };
+
+            var response = await DoPost(_endpoint, request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var result = responseData.RootElement.GetProperty("name").GetString();
+
+            result.Should().Be(_userName);
+        }
+    }
+}
