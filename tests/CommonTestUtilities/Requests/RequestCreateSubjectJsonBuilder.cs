4e4b82dabf6eaa2f54aa2e65052967f3409448b5
brunoolmrezende <brunoolmrezende303@gmail.com>
2025-02-25 17:10:07 -0300

    feat: add create subject validator test.

---
+++ tests/CommonTestUtilities/Requests/RequestCreateSubjectJsonBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using StudyFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestCreateSubjectJsonBuilder
+    {
+        public static RequestCreateSubjectJson Build()
+        {
+            return new Faker<RequestCreateSubjectJson>()
+                .RuleFor(subject => subject.Name, faker => faker.Commerce.ProductName());
+        }
+    }
+}
---
+++ tests/ValidatorTest/Subject/Create/CreateSubjectValidatorTest.cs
@@ -0,0 +1,37 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using StudyFlow.Application.UseCases.Subject.Create;
+using StudyFlow.Exceptions;
+
+namespace Validators.Test.Subject.Create
+{
+    public class CreateSubjectValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var request = RequestCreateSubjectJsonBuilder.Build();
+
+            var validator = new CreateSubjectValidator();
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+            result.Errors.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Error_Empty_Name()
+        {
+            var request = RequestCreateSubjectJsonBuilder.Build();
+            request.Name = string.Empty;
+
+            var validator = new CreateSubjectValidator();
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceMessagesException.NAME_EMPTY));
+        }
+    }
+}
